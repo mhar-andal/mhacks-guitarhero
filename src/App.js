472146e4d8c87f85a84d882c413b57cb9f206c86
Mhar Andal <mharandal96@gmail.com>
2017-03-25 02:22:06 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <img src="../public/FretBoard.jpg" id="bg" alt="" />
+      </div>
+    );
+  }
+}
+
+export default App;
